dbb753f7e1e564205f25618cbd9c46f96593f00e
Ralf Lopez <ralflopez3@gmail.com>
2021-06-18 18:00:18 +0800

    :sparkles: FEAT: define basic insert queries

---
+++ database/sql/insert.sql
@@ -0,0 +1,67 @@
+INSERT INTO users (
+    username,
+    email,
+    password
+) VALUES (
+    'test',
+    'test@gmail.com',
+    'hashedpass'  
+);
+
+INSERT INTO stories (
+    user_id,
+    path
+) VALUES (
+    1,
+    'stories url'
+);
+
+INSERT INTO posts (
+    user_id,
+    caption
+) VALUES (
+    1,
+    'first post'
+);
+
+INSERT INTO uploads (
+    post_id,
+    path
+) VALUES (
+    1,
+    'photo url'
+);
+
+INSERT INTO favorites (
+    post_id,
+    user_id
+) VALUES (
+    1,
+    1
+);
+
+INSERT INTO likes (
+    user_id,
+    post_id
+) VALUES (
+    1,
+    1
+);
+
+INSERT INTO comments (
+    user_id,
+    post_id,
+    comment
+) VALUES (
+    1,
+    1,
+    'thanks'
+);
+
+INSERT INTO replies (
+    target_id,
+    reply_id
+) VALUES (
+    1,
+    2
+);
