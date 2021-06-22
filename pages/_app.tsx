27fb70c02962f7d7b0c4ed4c68021f52c9fdd1ad
Ralf Lopez <ralflopez3@gmail.com>
2021-06-22 23:14:08 +0800

    :sparkles: FEAT: add redux store provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,20 @@ import type { AppProps } from 'next/app'
 import Layout from '../components/Layouts/Layout'
 import { Provider as NextAuthProvider } from 'next-auth/client'
 import '../styles/bootstrap.scss'
+import { Provider } from 'react-redux'
+import { useStore } from '../app/store'
 
 function MyApp({ Component, pageProps }: AppProps) {
+	const store = useStore(pageProps.initialReduxState)
+
 	return (
-		<Layout>
-			<NextAuthProvider session={pageProps.session}>
-				<Component {...pageProps} />
-			</NextAuthProvider>
-		</Layout>
+		<Provider store={store}>
+			<Layout>
+				<NextAuthProvider session={pageProps.session}>
+					<Component {...pageProps} />
+				</NextAuthProvider>
+			</Layout>
+		</Provider>
 	)
 }
 export default MyApp
